316c11bd4776c7255d9900b1b4979468ee7d8aed
DisabledMallis <mallisiscool@gmail.com>
2021-04-12 16:38:32 -0400

    So fabric already has a mapping parser

--- src/main/java/io/github/disabledmallis/map2cpp/Main.java
+++ src/main/java/io/github/disabledmallis/map2cpp/Main.java
@@ -1,7 +1,9 @@
 package io.github.disabledmallis.map2cpp;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
+    public static void main(String[] args) throws IOException {
+		Logger.Log("Hey");
     }
 }
