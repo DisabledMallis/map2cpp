c914a6930b739eabdd0c3587cf0ffdc07bdb3162
DisabledMallis <mallisiscool@gmail.com>
2021-04-19 17:32:22 -0400

    start reading mapping stuffs

---
+++ src/main/java/io/github/disabledmallis/map2cpp/Intermediary.java
@@ -0,0 +1,10 @@
+package io.github.disabledmallis.map2cpp;
+
+public class Intermediary {
+	public static void readTable(String intermediaryTable) {
+
+	}
+	public static String fromObfuscation(String obfuscated) {
+		return "";
+	}
+}
--- src/main/java/io/github/disabledmallis/map2cpp/Main.java
+++ src/main/java/io/github/disabledmallis/map2cpp/Main.java
@@ -1,9 +1,37 @@
 package io.github.disabledmallis.map2cpp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+
+import net.fabricmc.mapping.tree.ClassDef;
+import net.fabricmc.mapping.tree.TinyMappingFactory;
+import net.fabricmc.mapping.tree.TinyTree;
 
 public class Main {
+	static String intermediaryPath = "mappings/intermediary.tiny";
     public static void main(String[] args) throws IOException {
-		Logger.Log("Hey");
+		for(int i = 0; i < args.length; i++) {
+			String current = args[i];
+			if(current.equals("-i") || current.equals("--intermediary")) {
+				intermediaryPath = args[i+1];
+			}
+		}
+		BufferedReader reader = new BufferedReader(new FileReader(intermediaryPath));
+		TinyTree tree = TinyMappingFactory.loadLegacy(reader);
+		Map<String,ClassDef> namespaceMap = tree.getDefaultNamespaceClassMap();
+		for(String cl : namespaceMap.keySet()) {
+			try {
+				Logger.Log(cl 
+				+ " -> " + 
+				namespaceMap
+				.get(cl)
+				.getName(cl));
+			} catch (Exception ex) {
+				Logger.Log(cl + " -> " + "error");
+			}
+			
+		}
     }
 }
